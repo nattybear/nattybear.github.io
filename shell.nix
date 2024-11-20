425f193d5a5c2b9f9a0782c21f8e4d1ab0d3b8e7
Joonkyu Park <vpark45@gmail.com>
2024-11-20 22:32:07 +0900

    Update shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
-pkgs.haskellPackages.developPackage {
-  root = ./.;
-  name = "hakyll-site";
 
+pkgs.mkShell {
   buildInputs = [
     pkgs.haskellPackages.hakyll
-    pkgs.haskellPackages.ghc
     pkgs.haskellPackages.cabal-install
+    pkgs.haskellPackages.ghc
   ];
-
-  src = ./.;
 }
