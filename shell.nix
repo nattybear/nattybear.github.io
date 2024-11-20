1479f8b472b6b986d219510dda25cfc9fa212ee1
Joonkyu Park <vpark45@gmail.com>
2024-11-20 17:23:13 +0900

    Create shell.nix

---
+++ shell.nix
@@ -0,0 +1,13 @@
+{ pkgs ? import <nixpkgs> {} }:
+pkgs.haskellPackages.developPackage {
+  pname = "hakyll-site";
+  version = "0.1.0.0";
+
+  buildInputs = [
+    pkgs.haskellPackages.hakyll
+    pkgs.haskellPackages.ghc
+    pkgs.haskellPackages.cabal-install
+  ];
+
+  src = ./.;
+}
